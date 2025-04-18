b0d5599141d5008b42435b57cfe51ddb3d90ace8
mikesmith-createfuture <mike.smith@xdesign.com>
2025-04-18 09:42:50 +0100

    base project

---
+++ src/test/java/com/mike/springaichat/SpringaichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.springaichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaichatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
